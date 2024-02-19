49c07541f3396c8f277f040dbb6888fe7488a7a3
Nina Sutaeva <s0548952@gmail.com>
2024-02-19 19:29:05 +0100

    still working on the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :bookings
+  resources :flats
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
