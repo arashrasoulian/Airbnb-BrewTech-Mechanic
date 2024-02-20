9019608a0c1276ad6a368582cc07a9a65aaf5110
Nina Sutaeva <s0548952@gmail.com>
2024-02-20 11:30:15 +0100

    added Map in a homepage, but not finished yet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-      # List all flats
-      @flats = Flat.all
-      # Initialize new booking
-      @booking = Booking.new
+    # List all flats
+    @flats = Flat.all
+    # Initialize new booking
+    @booking = Booking.new
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
-  
 
 end
